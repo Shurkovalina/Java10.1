cd9c4d26732cffd5c3a5a48ca0d49db10826a3cd
Shurkovalina <shurkowalina@yandex.ru>
2021-01-31 10:35:51 +0300

    Test1

--- src/test/java/manager/ProductManagerTest.java
+++ src/test/java/manager/ProductManagerTest.java
@@ -9,15 +9,43 @@ import repository.ProductRepository;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductManagerTest {
-//    private ProductRepository repository = new ProductRepository();
 
     @Test
     public void search1() {
         ProductManager manager = new ProductManager(new ProductRepository());
-        manager.add(new Book(1, "book", 10, "a"));
-        manager.add(new Smartphone(2, "Smart", 100, "Samsung"));
+        Product product1 = new Book(1, "book1", 10, "author1");
+        Product product2 = new Book(2, "book2", 20, "author2");
+        Product product3 = new Book(3, "book3", 30, "author3");
+        Product product4 = new Smartphone(4, "Smart", 100, "Samsung");
 
-        System.out.println(manager.searchBy("Smart"));
-        System.out.println(manager.searchBy("book"));
+        manager.add(product1);
+        manager.add(product2);
+        manager.add(product3);
+        manager.add(product4);
+
+        Product[] expected = new Product[]{product3};
+        Product[] actual = manager.searchBy("book3");
+
+        assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void search2() {
+        ProductManager manager = new ProductManager(new ProductRepository());
+        Product product1 = new Book(1, "book1", 10, "author1");
+        Product product2 = new Book(2, "book2", 20, "author2");
+        Product product3 = new Book(3, "book3", 30, "author3");
+        Product product4 = new Smartphone(4, "Smart", 100, "Samsung");
+
+        manager.add(product1);
+        manager.add(product2);
+        manager.add(product3);
+        manager.add(product4);
+
+        Product[] expected = new Product[]{product4};
+        Product[] actual = manager.searchBy("Samsung");
+
+        assertArrayEquals(expected, actual);
+    }
+
 }
